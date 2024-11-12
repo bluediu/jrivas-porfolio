fd8096794b864b5620f95b734c9bf3ddfada8d88
jrivas <josuerivas2311@gmail.com>
2024-11-12 17:47:53 -0600

    Add download CV button

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,15 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'first-color': 'hsl(207, 90%, 61%)',
+        'first-color-alt': 'hsl(207, 90%, 57%)',
+      },
+      boxShadow: {
+        button: '0 8px 24px rgba(0, 112, 243, 0.25)',
+      },
+    },
   },
   plugins: [],
 };
